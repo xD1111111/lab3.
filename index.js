226b8e0a53113a12b76b27a1758d414ff29e8f56
xd <artem082022@gmail.com>
2026-04-04 17:07:08 +0300

    fix: add LRU eviction strategy

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 function memoize(fn, maxSize = Infinity) {
   const cache = new Map();
-
   return function(...args) {
     const key = JSON.stringify(args);
 
     if (cache.has(key)) {
+      const value = cache.get(key);
+      cache.delete(key);
+      cache.set(key, value); 
       console.log("From cache");
-      return cache.get(key);
+      return value;
     }
 
-    // якщо кеш переповнений — видаляємо найстаріший
     if (cache.size >= maxSize) {
       const firstKey = cache.keys().next().value;
       cache.delete(firstKey);
@@ -17,21 +18,17 @@ function memoize(fn, maxSize = Infinity) {
 
     console.log("Calculating...");
     const result = fn(...args);
-
     cache.set(key, result);
     return result;
   };
 }
+
 // ===== TEST =====
-
 const add = (a, b) => a + b;
-
 const memoAdd = memoize(add, 2);
 
 console.log(memoAdd(2, 3)); // Calculating...
-console.log(memoAdd(2, 3)); // From cache
-
 console.log(memoAdd(4, 5)); // Calculating...
-console.log(memoAdd(6, 7)); // Calculating...
-
-console.log(memoAdd(2, 3)); // Calculating (бо було видалено)
+console.log(memoAdd(2, 3)); // From cache 
+console.log(memoAdd(6, 7)); // Calculating... 
+console.log(memoAdd(2, 3)); // From cache 
